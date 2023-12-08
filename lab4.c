579f76d5059cd0ed0d10536c76a1c99aa8e88f77
annadirsha20 <144897320+annadirsha20@users.noreply.github.com>
2023-12-08 18:16:23 +0700

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-#define ST 42                                                                                        
-#define MAX 43
+#define ST 31
+#define MAX 42
 
-// Функция для сдвига битового массива на указанное количество позиций                               
-void replace(int result[], int replace) {
-    int res[MAX];  // Увеличиваем размер 
+// Функция для сдвига битового массива на указанное количество позиций
+void sdvig(int result[], int sdvig) {
+    int res[MAX];  // Увеличиваем размер на 3 для хранения сдвинутых битов
     for (int i = 0; i < MAX; i++) {
-        if (i < replace)
-            res[i] = result[i + ST - replace];
+        if (i < sdvig)
+            res[i] = result[i + ST - sdvig];
         else
-            res[i] = result[i - replace];
+            res[i] = result[i - sdvig];
     }
 
     // Копируем результат сдвига обратно в массив result
@@ -20,43 +20,40 @@ void replace(int result[], int replace) {
 }
 
 // Функция для вычисления автокорреляции
-double correlat(int origin[], int move[], int length) {
+double autocorrelation(int original[], int shifted[], int length, int shift) {
     int matchCount = 0;
 
     // Подсчет числа совпадающих бит
     for (int i = 0; i < length; i++) {
-        if (origin[i] == move[i]) {
+        if (original[i] == shifted[i]) {
             matchCount++;
         }
     }
 
     // Вычисление автокорреляции
-    double p  = (double)1/(length);
-    return (1/ST)*(matchCount - (length - matchCount));
-    
+    return (double)(matchCount - length / 2) / (length / 2);
 }
-
-
 
 int main() {
 
     // Исходные значения 1 задание
-    int x1 = 1, x2 = 1, x3 = 0, x4 = 0, x5 = 0;
-    int y1 = 1, y2 = 1, y3 = 1, y4 = 1, y5 = 1;
+    int x1 = 1, x2 = 0, x3 = 0, x4 = 0, x5 = 0;
+    int y1 = 1, y2 = 1, y3 = 0, y4 = 1, y5 = 1;
 
-    // int x1 = 1, x2 = 1, x3 = 1, x4 = 1, x5 = 0;
-    // int y1 = 1, y2 = 1, y3 = 0, y4 = 0, y5 = 0;
+    
+    // int x1 = 1, x2 = 1, x3 = 0, x4 = 0, x5 = 1;
+    // int y1 = 1, y2 = 1, y3 = 1, y4 = 0, y5 = 0;
 
     // Массивы для хранения оригинальной и сдвинутой последовательностей
-    int origin[MAX];
-    int move[MAX];
+    int original[MAX];
+    int shifted[MAX];
 
     // Заполнение массива результатов операции XOR и сохранение оригинала
     for (int i = 0; i < MAX; i++) {
-        origin[i] = x5 ^ y5;
-        move[i] = x5 ^ y5;
+        original[i] = x5 ^ y5;
+        shifted[i] = x5 ^ y5;
 
-        int sumx = x1 ^ x3;
+        int sumx = x3 ^ x4;
         x5 = x4; x4 = x3; x3 = x2; x2 = x1;
         x1 = sumx;
 
@@ -66,7 +63,7 @@ int main() {
     }
 
     // Вывод заголовка таблицы
-    printf("Сдвиг| Биты     | Автокорреляция\n");
+    printf("Сдвиг|                     Биты                   | Автокорреляция\n");
 
     // Вывод строк таблицы
     for (int shift = 0; shift < MAX; shift++) {
@@ -74,17 +71,17 @@ int main() {
 
         // Вывод битов оригинала
         for (int i = 0; i < ST; i++) {
-            printf("%d", move[i]);
+            printf("%d", shifted[i]);
         }
 
         printf(" | ");
 
-        //  вычисление автокорреляции
-        double corr = correlat(origin, move, ST);
+        // Сдвиг и вычисление автокорреляции
+        double corr = autocorrelation(original, shifted, ST, shift);
 
         printf("%+1.3f\n", corr);
-        // replace
-        replace(move, 1);
+
+        sdvig(shifted, 1);
     }
 
     return 0;
